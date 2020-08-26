e77b90eb5ce2bdf058f2ba5b15510468e0a286da
Justin Chau <chausicle@gmail.com>
2020-08-26 13:36:28 -0700

    Add person.js and util.js for import export practice

--- public/bundle.js
+++ public/bundle.js
@@ -60,14 +60,42 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */
-/***/ (function(module, exports) {
+/* 0 */,
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log("app.js is running!!!");
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// import "./utils.js";
+// import { square, add } from "./utils.js";
+
+// console.log("app.js is running!!!");
+// console.log(square(4));
+// console.log(add(100, 23));
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](18))
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18))
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+console.log("person.js is running");
+
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+const canDrink = (age) => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 
 /***/ })
