595a37d5e5a464733b30b333a63ff3ee44345bf4
Justin Chau <chausicle@gmail.com>
2020-08-26 16:23:29 -0700

    Setup webpack to run babel on js files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log(validator.isEmail("test@gmail.com"));
+const template = <p>this is jsx from webpack</p>
+ReactDOM.render(template, document.querySelector("#app"));
