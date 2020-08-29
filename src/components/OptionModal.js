f2af5283efd61b225172ba2ef48a9f27b9edd418
Justin Chau <chausicle@gmail.com>
2020-08-29 01:28:56 -0700

    Style 3rd party Modal with higher style specificity

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,13 +3,19 @@ import Modal from "react-modal";
 
 const OptionModal = (props) => (
   <Modal
+    className="modal"
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.handleClearSelectedOption}
+    closeTimoutMS={200}
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 
